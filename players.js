bfa719d879958ceac5e72b11aa43903e5843a04a
tdooher11 <tim.dooher@gmail.com>
2026-01-23 12:06:33 -0800

    Add files via upload
    initial

---
+++ players.js
@@ -0,0 +1,92 @@
+const PLAYERS = [
+  {
+    name: "Lionel Messi",
+    country: "Argentina",
+    currentClub: "Inter Miami",
+    clubs: ["Barcelona","PSG","Inter Miami"],
+    awards: ["World Cup 2022","Copa América","Ballon d'Or"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/e/eb/Messi_2025.jpg",
+    fotmobUrl: "https://www.fotmob.com/players/30981/lionel-messi"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    country: "Portugal",
+    currentClub: "Al-Nassr",
+    clubs: ["Sporting","Manchester United","Real Madrid","Juventus","Al-Nassr"],
+    awards: ["Euro 2016","Champions League x5","Ballon d'Or"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/44/Cristiano_Ronaldo_0876.jpg",
+    fotmobUrl: "https://www.fotmob.com/players/30893/cristiano-ronaldo"
+  },
+  {
+    name: "Kylian Mbappé",
+    country: "France",
+    currentClub: "Real Madrid",
+    clubs: ["Monaco","PSG","Real Madrid"],
+    awards: ["World Cup 2018","Ligue 1 titles"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b8/Kylian_Mbapp%C3%A9_en_visite_%C3%A0_Caen.jpg",
+    fotmobUrl: "https://www.fotmob.com/players/701154/kylian-mbappe"
+  },
+  {
+    name: "Erling Haaland",
+    country: "Norway",
+    currentClub: "Manchester City",
+    clubs: ["Molde","RB Salzburg","Borussia Dortmund","Manchester City"],
+    awards: ["Champions League","Premier League Golden Boot"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/71/Erling_Haaland_June_2025.jpg",
+    fotmobUrl: "https://www.fotmob.com/players/737066/erling-haaland"
+  },
+  {
+    name: "Jude Bellingham",
+    country: "England",
+    currentClub: "Real Madrid",
+    clubs: ["Birmingham City","Borussia Dortmund","Real Madrid"],
+    awards: ["Champions League","La Liga"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/8/88/Jude_Bellingham_Laureus_2024_%28cropped2%29.jpg",
+    fotmobUrl: "https://www.fotmob.com/players/1077894/jude-bellingham"
+  },
+  {
+    name: "Mohamed Salah",
+    country: "Egypt",
+    currentClub: "Liverpool",
+    clubs: ["Basel","Chelsea","Roma","Liverpool"],
+    awards: ["Champions League","Premier League"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/03/Mohamed_Salah.JPG",
+    fotmobUrl: "https://www.fotmob.com/players/292462/mohamed-salah"
+  },
+  {
+    name: "Lamine Yamal",
+    country: "Spain",
+    currentClub: "Barcelona",
+    clubs: ["Barcelona"],
+    awards: ["Euro 2024","La Liga"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/d/df/Lamine_Yamal_in_2025_%28cropped2%29.jpg",
+    fotmobUrl: "https://www.fotmob.com/players/1467236/lamine-yamal"
+  },
+  {
+    name: "Neymar Jr.",
+    country: "Brazil",
+    currentClub: "Al-Hilal",
+    clubs: ["Santos","Barcelona","PSG","Al-Hilal"],
+    awards: ["Champions League","Copa Libertadores","Olympic Gold"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Neymar_2018.jpg",
+    fotmobUrl: "https://www.fotmob.com/players/30890/neymar"
+  },
+  {
+    name: "Vinícius Júnior",
+    country: "Brazil",
+    currentClub: "Real Madrid",
+    clubs: ["Flamengo","Real Madrid"],
+    awards: ["Champions League","La Liga"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/98/Vinicius_Junior_2023.jpg",
+    fotmobUrl: "https://www.fotmob.com/players/868012/vinicius-junior"
+  },
+  {
+    name: "Harry Kane",
+    country: "England",
+    currentClub: "Bayern Munich",
+    clubs: ["Tottenham Hotspur","Bayern Munich"],
+    awards: ["World Cup Golden Boot 2018","Bundesliga Golden Boot"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Harry_Kane_2023.jpg",
+    fotmobUrl: "https://www.fotmob.com/players/36249/harry-kane"
+  }
+];
